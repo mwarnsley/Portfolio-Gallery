1517f0fc7eff793fe01b27a5ca5529eb58d75c12
Warnsley <Marcus.Warnsley@navient.com>
2016-04-07 14:58:37 -0400

    Adding some javascript for the clicking of the links and changing the portfolio piecces

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-ECHO is on.
+$(document).ready(function(){
+
+	$("nav").on("click", "a", function(){
+		// assign the current class
+		$("nav li.current").removeClass("current");
+		$(this).parent().addClass("current");
+
+	// set the heading text
+	$("h1#heading").text($(this).text());
+
+	// Get & filter link text
+	var category = $(this).text().toLowerCase().replace(' ','-');
+
+	//remove hidden class if all-projects is selected
+	if(category === 'all-projects'){
+		$("ul#gallery li:hidden").fadeIn("slow").removeClass("hidden");
+	}else{
+		$("ul#gallery li").each(function(){
+			if(!$(this).hasClass(category)){
+				$(this).hide().addClass("hidden");
+			}else{
+				$(this).fadeIn('slow').removeClass('hidden');
+			}
+		})
+	}
+
+	// stopping the link behavior
+	return false;
+
+	});
+
+
+});
